6232b4f658b863266e9bb822bdcd9218f5acf4b3
jrodriguez225 <98556914+jrodriguez225@users.noreply.github.com>
2022-10-06 13:18:46 +0200

    Logging simple

--- server/src/database.js
+++ server/src/database.js
@@ -5,7 +5,7 @@ const URI = 'mongodb://localhost/moodex';
 mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+    })
     .then((db) => console.log('db is connected'))
     .catch((err) => console.error(err));
 
