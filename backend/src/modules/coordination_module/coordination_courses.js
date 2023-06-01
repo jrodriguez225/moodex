34ba445dff1926745947d22a1fe8449a5cee1277
jrodriguez225 <jrodriguez225@ikasle.ehu.eus>
2023-06-01 17:31:21 +0200

    Prototipo 4 - Detallar calendario

--- backend/src/modules/coordination_module/coordination_courses.js
+++ backend/src/modules/coordination_module/coordination_courses.js
@@ -11,19 +11,16 @@ cooordination_courses.getTeacherCourses = (token, userid) => {
             // lista que almacena los cursos en los que el usuario tiene rol de profesor
             const teacherCourses = [];
             // obtener los cursos del usuario
-            let response = await data_users.getUserCourses(token, userid);
+            const response = await data_users.getUserCourses(token, userid);
             const userCourses = response.data;
             // por cada curso del usuario
             for (let coursesIndex = 0; coursesIndex < userCourses.length; coursesIndex++) {
                 const course = userCourses[coursesIndex];
                 const courseid = course.id;
-                // obtener los usuarios del curso
-                response = await data_courses.getCourseUsers(token, courseid);
-                const users = response.data;
-                // obtener el usuario actual a partir de la lista de usuarios del curso
-                const user = users.filter(user => userid === user.id)[0];
+                // obtener los roles del usuario en el curso
+                const roles = await utils.getCourseUserRoles(token, courseid, userid);
                 // mirar si tiene rol de profesor
-                const isTeacher = user.roles.some(role => role.shortname === 'editingteacher' || role.shortname === 'teacher');
+                const isTeacher = roles.some(role => role.shortname === 'editingteacher' || role.shortname === 'teacher');
                 // si lo tiene
                 if (isTeacher) {
                     // añadir el curso a la lista
@@ -58,7 +55,7 @@ cooordination_courses.getCourseStudentsCalendarEvents = (token, courseid, course
             for (let usersIndex = 0; usersIndex < users.length; usersIndex++) {
                 const user = users[usersIndex];
                 // mirar si tiene rol de alumno
-                const isStudent = user.roles.some(role => role.shortname === 'student');
+                let isStudent = user.roles.some(role => role.shortname === 'student');
                 // si lo tiene
                 if (isStudent) {
                     // incrementar el número de alumnos
@@ -70,23 +67,30 @@ cooordination_courses.getCourseStudentsCalendarEvents = (token, courseid, course
                     // por cada curso del alumno
                     for (let coursesIndex = 0; coursesIndex < courses.length; coursesIndex++) {
                         const course = courses[coursesIndex];
-                        // obtener los eventos de calendario del curso
-                        response = await data_courses.getCourseCalendarEvents(token, course.id, coursestartdate, courseenddate);
-                        const courseEvents = response.data.events;
-                        // por cada evento de calendario del curso
-                        for (let eventsIndex = 0; eventsIndex < courseEvents.length; eventsIndex++) {
-                            const event = courseEvents[eventsIndex];
-                            // si el evento de calendario es visible y es un evento de curso
-                            if (event.visible === 1 && event.eventtype === 'course') {
-                                // obtener las fechas del evento de calendario
-                                const eventDates = utils.getDates(event.timestart, event.timeduration);
-                                // por cada fecha del evento de calendario
-                                for (let eventDatesIndex = 0; eventDatesIndex < eventDates.length; eventDatesIndex++) {
-                                    const eventDate = eventDates[eventDatesIndex];
-                                    // si no está ya en la lista del alumno
-                                    if (!dates.some(date => utils.equalDates(eventDate, date))) {
-                                        // añadir la fecha a la lista del alumno
-                                        dates.push(eventDate);
+                        // obtener los roles del alumno en el curso
+                        const roles = await utils.getCourseUserRoles(token, course.id, user.id);
+                        // mirar si tiene rol de alumno en el curso
+                        isStudent = roles.some(role => role.shortname === 'student');
+                        // si lo tiene
+                        if (isStudent) {
+                            // obtener los eventos de calendario del curso
+                            response = await data_courses.getCourseCalendarEvents(token, course.id, coursestartdate, courseenddate);
+                            const courseEvents = response.data.events;
+                            // por cada evento de calendario del curso
+                            for (let eventsIndex = 0; eventsIndex < courseEvents.length; eventsIndex++) {
+                                const event = courseEvents[eventsIndex];
+                                // si el evento de calendario es visible y es un evento de curso
+                                if (event.visible === 1 && event.eventtype === 'course') {
+                                    // obtener las fechas del evento de calendario
+                                    const eventDates = utils.getDates(event.timestart, event.timeduration);
+                                    // por cada fecha del evento de calendario
+                                    for (let eventDatesIndex = 0; eventDatesIndex < eventDates.length; eventDatesIndex++) {
+                                        const eventDate = eventDates[eventDatesIndex];
+                                        // si no está ya en la lista del alumno
+                                        if (!dates.some(date => utils.equalDates(eventDate, date))) {
+                                            // añadir la fecha a la lista del alumno
+                                            dates.push(eventDate);
+                                        }
                                     }
                                 }
                             }
--- backend/src/work_tools/utils.js
+++ backend/src/work_tools/utils.js
@@ -1,3 +1,5 @@
+const data_courses = require('../modules/data_module/data_courses');
+
 const utils = {};
 
 utils.headerFormat = (header) => {
@@ -28,7 +30,7 @@ utils.isNaturalNumber = (number) => {
 };
 
 utils.getDate = (unix_timestamp) => {
-    const date = new Date(unix_timestamp * 1000 + 28800000);
+    const date = new Date((unix_timestamp + 28800) * 1000);
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
@@ -37,8 +39,8 @@ utils.getDate = (unix_timestamp) => {
 
 utils.getDates = (date, duration) => {
     const dates = [];
-    const currentDate = new Date(date * 1000 + 28800000);
-    const stopDate = new Date((date + duration) * 1000 + 28800000);
+    const currentDate = new Date((date + 28800) * 1000);
+    const stopDate = new Date((date + duration + 28800) * 1000);
     while (currentDate <= stopDate) {
         const year = currentDate.getFullYear();
         const month = currentDate.getMonth() + 1;
@@ -51,6 +53,20 @@ utils.getDates = (date, duration) => {
 
 utils.equalDates = (date1, date2) => {
     return date1.year === date2.year && date1.month === date2.month && date1.day === date2.day;
+};
+
+// obtener los roles del usuario en el curso
+utils.getCourseUserRoles = async (token, courseid, userid) => {
+    try {
+        // obtener los usuarios del curso
+        const response = await data_courses.getCourseUsers(token, courseid);
+        const users = response.data;
+        // obtener el usuario actual a partir de la lista de usuarios del curso
+        const user = users.filter(user => userid === user.id)[0];
+        // devolver los roles del usuario en el curso
+        return user.roles;
+    }
+    catch (error) {}
 };
 
 module.exports = utils;
